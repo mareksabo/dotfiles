612f774ebde742f8a4750d7093d97ac48454d56a
Marek Šabo <marek.sabo@firma.seznam.cz>
2018-11-19 15:55:49 +0100

    Update

--- zsh-install.sh
+++ zsh-install.sh
@@ -8,9 +8,11 @@ chsh -s zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # Download zshrc
-wget https://raw.githubusercontent.com/mareksabo/dotfiles/master/.zshrc
+wget https://raw.githubusercontent.com/mareksabo/dotfiles/master/.zshrc .zshrc-temp
+mv .zshrc-temp .zshrc
 
 # Clone the plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+rm zsh-install.sh
