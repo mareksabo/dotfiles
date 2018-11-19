5d5f33c6d2992b65354259869a4c902714a3c8ab
Marek Šabo <marek.sabo@firma.seznam.cz>
2018-11-19 16:32:15 +0100

    Correct zsh path

--- zsh-install.sh
+++ zsh-install.sh
@@ -2,7 +2,7 @@
 
 # Install zsh
 sudo apt-get install zsh -y
-chsh -s zsh
+chsh -s /usr/bin/zsh
 
 # Install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
